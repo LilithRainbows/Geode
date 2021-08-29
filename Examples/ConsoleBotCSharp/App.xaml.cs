6fd49a18a93d2ee96345359a9f5f9215eb727886
Lilith <lilithrainbows@outlook.com>
2021-08-29 14:13:30 -0300

    Windowless ConsoleBot examples

--- Examples/ConsoleBotCSharp/App.xaml.cs
+++ Examples/ConsoleBotCSharp/App.xaml.cs
@@ -1,17 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Geode.Extension;
+using System;
 using System.Windows;
 
 namespace ConsoleBotCSharp
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public ConsoleBot ConsoleBot;
+        public GeodeExtension Extension;
+
+        public App()
+        {
+            Extension = new GeodeExtension("ConsoleBotCSharp", "Geode examples.", "Lilith"); //Instantiate extension
+            //Add extension event handlers
+            Extension.OnCriticalErrorEvent += Extension_OnCriticalErrorEvent;
+            //
+            Extension.Start(); //Start extension
+            ConsoleBot = new ConsoleBot(Extension, "CSharp example"); //Instantiate a new ConsoleBot
+            //Add ConsoleBot event handlers
+            ConsoleBot.OnMessageReceived += ConsoleBot_OnMessageReceived;
+            ConsoleBot.OnBotLoaded += ConsoleBot_OnBotLoaded; ;
+            //
+            ConsoleBot.ShowBot(); //Show ConsoleBot
+        }
+
+        public void BotWelcome()
+        {
+            ConsoleBot.BotSendMessage("Welcome |");
+            ConsoleBot.BotSendMessage("Use /help to get info.");
+        }
+
+        private void ConsoleBot_OnBotLoaded(string e)
+        {
+            BotWelcome(); //Show welcome message when ConsoleBot loaded
+        }
+
+        private void ConsoleBot_OnMessageReceived(string e)
+        {
+            switch (e.ToLower()) //Handle received message
+            {
+                case "/help":
+                    {
+                        ConsoleBot.BotSendMessage("Commands:");
+                        ConsoleBot.BotSendMessage("/look1 and /look2 to change current look.");
+                        ConsoleBot.BotSendMessage("/sit to force sit.");
+                        ConsoleBot.BotSendMessage("/fx to get light sabber fx.");
+                        ConsoleBot.BotSendMessage("/exit to exit extension.");
+                        break;
+                    }
+
+                case "/look1":
+                    {
+                        Extension.SendToServerAsync(Extension.Out.UpdateFigureData, "F", "hr-515-45.ch-665-71.lg-3216-73.hd-600-10.fa-3276-72");
+                        break;
+                    }
+
+                case "/look2":
+                    {
+                        Extension.SendToServerAsync(Extension.Out.UpdateFigureData, "M", "hr-893-45.ch-235-71.lg-3290-82.hd-180-10.fa-3276-72");
+                        break;
+                    }
+
+                case "/sit":
+                    {
+                        Extension.SendToServerAsync(Extension.Out.ChangePosture, 1);
+                        break;
+                    }
+
+                case "/fx":
+                    {
+                        Extension.SendToServerAsync(Extension.Out.Chat, ":yyxxabxa", 0, -1);
+                        break;
+                    }
+
+                default:
+                    {
+                        BotWelcome();
+                        break;
+                    }
+            }
+        }
+
+        private void Extension_OnCriticalErrorEvent(string e)
+        {
+            MessageBox.Show(e + ".", "Critical error", MessageBoxButton.OK, MessageBoxImage.Error); //Show extension critical error
+            Environment.Exit(0);
+        }
     }
 }
--- Examples/LTDHelper/Application.xaml.vb
+++ Examples/LTDHelper/Application.xaml.vb
@@ -1,6 +1,110 @@
-﻿Class Application
+﻿Imports Geode.Extension
+Imports Geode.Network
 
-    ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
-    ' can be handled in this file.
+Class Application
+    Public WithEvents Extension As GeodeExtension
+    Public WithEvents ConsoleBot As ConsoleBot
+    Public TaskStarted As Boolean = False
+    Public TaskBlocked As Boolean = False
 
+    Public Sub New()
+        Extension = New GeodeExtension("LTDHelper", "Geode examples.", "Lilith") 'Instantiate extension
+        Extension.Start() 'Start extension
+        ConsoleBot = New ConsoleBot(Extension, "LTDHelper") 'Instantiate a new ConsoleBot
+        ConsoleBot.ShowBot() 'Show ConsoleBot
+    End Sub
+
+    Sub BotWelcome()
+        ConsoleBot.BotSendMessage("Welcome |")
+        ConsoleBot.BotSendMessage("Use /start or /stop")
+    End Sub
+
+    Async Function TryToBuyLTD() As Task
+        If TaskStarted Then
+            Try
+                Extension.SendToServerAsync(Extension.Out.GetCatalogIndex, "NORMAL")
+                Dim CatalogIndexData = Await Extension.WaitForPacketAsync(Extension.In.CatalogIndex, 1000)
+                Dim CatalogRoot As New Geode.Habbo.Packages.HCatalogNode(CatalogIndexData.Packet)
+                Dim LTDCategory = FindCatalogCategory(CatalogRoot.Children, "ler") 'You can test with other category like: set_mode
+                Extension.SendToServerAsync(Extension.Out.PurchaseFromCatalog, LTDCategory.PageId, LTDCategory.OfferIds(0), "", 1)
+                If Await Extension.WaitForPacketAsync(Extension.In.PurchaseOK, 500) IsNot Nothing Then
+                    ConsoleBot.BotSendMessage("Successfully purchased an LTD.")
+                    TaskBlocked = True
+                    TaskStarted = False
+                Else
+                    Throw New Exception("LTD not purchased!")
+                End If
+            Catch
+                TryToBuyLTD()
+            End Try
+        End If
+    End Function
+
+    Private Function FindCatalogCategory(NodeChildrens As Geode.Habbo.Packages.HCatalogNode(), CategoryName As String) As Geode.Habbo.Packages.HCatalogNode
+        For Each NodeChild In NodeChildrens
+            If NodeChild.PageName = CategoryName Then
+                Return NodeChild
+            Else
+                Dim RecursiveSearchResult = FindCatalogCategory(NodeChild.Children, CategoryName)
+                If RecursiveSearchResult IsNot Nothing Then
+                    Return RecursiveSearchResult
+                End If
+            End If
+        Next
+        Return Nothing
+    End Function
+
+    Private Sub ConsoleBot_OnBotLoaded(e As String) Handles ConsoleBot.OnBotLoaded
+        BotWelcome() 'Show welcome message when ConsoleBot loaded
+    End Sub
+
+    Private Sub ConsoleBot_OnMessageReceived(e As String) Handles ConsoleBot.OnMessageReceived
+        If TaskBlocked = False Then
+            Select Case e.ToLower 'Handle received message
+                Case "/start"
+                    If TaskStarted = False Then
+                        ConsoleBot.BotSendMessage("Ok, i will try to buy the LTD, you can use /stop to finish.")
+                        TaskStarted = True
+                        TryToBuyLTD()
+                    End If
+                Case "/stop"
+                    If TaskStarted Then
+                        ConsoleBot.BotSendMessage("Stopped, you can use /start to try again.")
+                        TaskStarted = False
+                    End If
+                Case Else
+                    BotWelcome()
+            End Select
+        Else
+            ConsoleBot.BotSendMessage("Use /exit to finish.")
+        End If
+    End Sub
+
+    Private Sub Extension_OnDataInterceptEvent(e As DataInterceptedEventArgs) Handles Extension.OnDataInterceptEvent
+        If Extension.In.ErrorReport.Match(e) Or Extension.In.PurchaseError.Match(e) Or Extension.In.PurchaseNotAllowed.Match(e) Or Extension.In.NotEnoughBalance.Match(e) Then 'Ignore common purchase errors
+            If TaskStarted = True Then
+                e.IsBlocked = True
+            End If
+        End If
+    End Sub
+
+    Private Sub Extension_OnCriticalErrorEvent(e As String) Handles Extension.OnCriticalErrorEvent
+        MsgBox(e & ".", MsgBoxStyle.Critical, "Critical error") 'Show extension critical error
+        Environment.Exit(0)
+    End Sub
 End Class
+
+Module SingleInstance
+    Sub Main()
+        Dim noPreviousInstance As Boolean
+
+        Using m As New Threading.Mutex(True, "LTDHelper for Geode", noPreviousInstance)
+            If Not noPreviousInstance Then
+                MessageBox.Show("Extension is already started!", "Error", MessageBoxButton.OK, MessageBoxImage.Error)
+            Else
+                Dim app As New Application()
+                app.Run()
+            End If
+        End Using
+    End Sub
+End Module
